67c878c6cf2454376347c096dc324d1622cbb922
exromany <exromany@lido.fi>
2025-08-01 12:11:12 +0300

    fix: rewards v2 as array

--- packages/csm-sdk/src/rewards-sdk/types.ts
+++ packages/csm-sdk/src/rewards-sdk/types.ts
@@ -78,7 +78,7 @@ export type RewardsReportV2 = {
   >;
 };
 
-export type RewardsReport = RewardsReportV1 | RewardsReportV2;
+export type RewardsReport = RewardsReportV1 | RewardsReportV2[];
 
 export type OperatorRewards = {
   distributed: bigint;
@@ -102,49 +102,69 @@ const rewardsReportV1Schema = z.object({
   distributable: z.number(),
   frame: z.tuple([z.number(), z.number()]),
   threshold: z.number(),
-  operators: z.record(z.object({
-    distributed: z.number(),
-    validators: z.record(z.object({
-      perf: z.object({
-        assigned: z.number(),
-        included: z.number(),
-      }),
-      slashed: z.boolean(),
-    })),
-  })),
-});
-
-const rewardsReportV2Schema = z.object({
-  blockstamp: blockstampSchema,
-  distributable: z.number(),
-  distributed_rewards: z.number(),
-  rebate_to_protocol: z.number(),
-  frame: z.tuple([z.number(), z.number()]),
-  operators: z.record(z.object({
-    distributed_rewards: z.number(),
-    performance_coefficients: z.object({
-      attestations_weight: z.number(),
-      blocks_weight: z.number(),
-      sync_weight: z.number(),
+  operators: z.record(
+    z.object({
+      distributed: z.number(),
+      validators: z.record(
+        z.object({
+          perf: z.object({
+            assigned: z.number(),
+            included: z.number(),
+          }),
+          slashed: z.boolean(),
+        }),
+      ),
     }),
-    validators: z.record(z.object({
-      attestation_duty: z.object({ assigned: z.number(), included: z.number() }),
-      distributed_rewards: z.number(),
-      performance: z.number(),
-      proposal_duty: z.object({ assigned: z.number(), included: z.number() }),
-      rewards_share: z.number(),
-      slashed: z.boolean(),
-      strikes: z.number(),
-      sync_duty: z.object({ assigned: z.number(), included: z.number() }),
-      threshold: z.number(),
-    })),
-  })),
+  ),
 });
 
-export const isRewardsReportV1 = (report: RewardsReport): report is RewardsReportV1 => {
+const rewardsReportV2Schema = z.array(
+  z.object({
+    blockstamp: blockstampSchema,
+    distributable: z.number(),
+    distributed_rewards: z.number(),
+    rebate_to_protocol: z.number(),
+    frame: z.tuple([z.number(), z.number()]),
+    operators: z.record(
+      z.object({
+        distributed_rewards: z.number(),
+        performance_coefficients: z.object({
+          attestations_weight: z.number(),
+          blocks_weight: z.number(),
+          sync_weight: z.number(),
+        }),
+        validators: z.record(
+          z.object({
+            attestation_duty: z.object({
+              assigned: z.number(),
+              included: z.number(),
+            }),
+            distributed_rewards: z.number(),
+            performance: z.number(),
+            proposal_duty: z.object({
+              assigned: z.number(),
+              included: z.number(),
+            }),
+            rewards_share: z.number(),
+            slashed: z.boolean(),
+            strikes: z.number(),
+            sync_duty: z.object({ assigned: z.number(), included: z.number() }),
+            threshold: z.number(),
+          }),
+        ),
+      }),
+    ),
+  }),
+);
+
+export const isRewardsReportV1 = (
+  report: RewardsReport,
+): report is RewardsReportV1 => {
   return rewardsReportV1Schema.safeParse(report).success;
 };
 
-export const isRewardsReportV2 = (report: RewardsReport): report is RewardsReportV2 => {
+export const isRewardsReportV2 = (
+  report: RewardsReport,
+): report is RewardsReportV2[] => {
   return rewardsReportV2Schema.safeParse(report).success;
 };
