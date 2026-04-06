53ca14bb1dd7d5e04e37c87670b82e40d0aa11d9
exromany <exromany@lido.fi>
2026-04-06 14:25:49 +0300

    fix: add OPERATOR_TYPE.IDVTC

--- packages/csm-sdk/src/common/constants/operator-types.ts
+++ packages/csm-sdk/src/common/constants/operator-types.ts
@@ -2,6 +2,7 @@ export enum OPERATOR_TYPE {
   DEF = 'DEF',
   LEA = 'LEA',
   ICS = 'ICS',
+  IDVTC = 'IDVTC',
   CC = 'CC',
 }
 
@@ -10,5 +11,6 @@ export const OPERATOR_TYPE_CURVE_ID = {
   [OPERATOR_TYPE.DEF]: 0n,
   [OPERATOR_TYPE.LEA]: 1n,
   [OPERATOR_TYPE.ICS]: 2n,
+  [OPERATOR_TYPE.IDVTC]: 3n,
   [OPERATOR_TYPE.CC]: undefined,
 } as const;
