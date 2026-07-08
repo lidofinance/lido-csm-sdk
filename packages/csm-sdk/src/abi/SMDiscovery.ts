e3c5c0b57261fea9886a01c3a16644c8bad82e18
exromany <exromany@lido.fi>
2026-07-08 17:15:27 +0300

    feat: update sm-discovery contract

--- packages/csm-sdk/src/abi/SMDiscovery.ts
+++ packages/csm-sdk/src/abi/SMDiscovery.ts
@@ -334,6 +334,67 @@ export const SMDiscoveryAbi = [
   },
   {
     type: 'function',
+    name: 'getOperatorsByCurveId',
+    inputs: [
+      {
+        name: '_moduleId',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+      {
+        name: '_curveId',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+      {
+        name: '_offset',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+      {
+        name: '_limit',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+    ],
+    outputs: [
+      {
+        name: '',
+        type: 'tuple[]',
+        internalType: 'struct NodeOperatorShort[]',
+        components: [
+          {
+            name: 'id',
+            type: 'uint256',
+            internalType: 'uint256',
+          },
+          {
+            name: 'managerAddress',
+            type: 'address',
+            internalType: 'address',
+          },
+          {
+            name: 'rewardAddress',
+            type: 'address',
+            internalType: 'address',
+          },
+          {
+            name: 'extendedManagerPermissions',
+            type: 'bool',
+            internalType: 'bool',
+          },
+          {
+            name: 'curveId',
+            type: 'uint256',
+            internalType: 'uint256',
+          },
+        ],
+      },
+    ],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
     name: 'getOperatorsWithLockedBond',
     inputs: [
       {
